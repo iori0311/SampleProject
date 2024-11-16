594a08e6d9d5b410d62bca1c8948a3684e1faf47
久保伊織 <xxaabassaaxx@gmail.com>
2024-11-16 16:07:56 +0900

    step2

--- SampleProject/ContentView.swift
+++ SampleProject/ContentView.swift
@@ -8,12 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var str = "Hello, Swift UI"
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(str)
+                .foregroundColor(.red)
+            Button("ボタン"){
+                str = "goodBye, Swift UI"
+            }
         }
         .padding()
     }
