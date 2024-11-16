67d7be96d1526fd077726def560dd4abd855ad1c
久保伊織 <xxaabassaaxx@gmail.com>
2024-11-16 15:47:10 +0900

    Initial Commit

---
+++ SampleProject/SampleProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleProjectApp.swift
+//  SampleProject
+//
+//  Created by 久保伊織 on 2024/11/16.
+//
+
+import SwiftUI
+
+@main
+struct SampleProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SampleProjectTests/SampleProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  SampleProjectTests.swift
+//  SampleProjectTests
+//
+//  Created by 久保伊織 on 2024/11/16.
+//
+
+import Testing
+@testable import SampleProject
+
+struct SampleProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
